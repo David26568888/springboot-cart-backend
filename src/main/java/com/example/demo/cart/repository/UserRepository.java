c6fe584f09423ddf908957024d9e9bf7b71a9624
FP <a2775255@gmail.com>
2025-10-02 21:24:32 +0800

    JavaSpringBoot day10 springboot-cart-backend

--- src/main/java/com/example/demo/cart/repository/UserRepository.java
+++ src/main/java/com/example/demo/cart/repository/UserRepository.java
@@ -4,11 +4,16 @@ package com.example.demo.cart.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 import com.example.demo.cart.model.entity.User;
+
+import jakarta.transaction.Transactional;
+
 import java.util.List;
 
 
@@ -18,6 +23,21 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	Optional<User> findFirstByUsername(String username);//屬於單筆查詢
 	
+	//利用原生sql
 	@Query(value = "select * from user where username = :username", nativeQuery=true)
 	Optional<User> findByUsernameNative(@Param("username") String username);
+	
+	//利用原生
+	@Modifying
+	@Transactional
+	@Query(value="update user set password = :password where id = :id",nativeQuery = true)
+	int updatePasswordByIdNative(@Param("id") Long id,@Param("password")String password);
+
+	//透過PQL(SQL語法+物件操作)
+	//nativeQuery 預設false 利用PQL寫法 如果是True就是用SQL原生
+	@Modifying
+	@Transactional
+	@Query(value = "update User u set u.password = :password where u.id= :id ")
+	int updatePasswordById(@Param("id") Long id,@Param("password")String password);
+	
 }
---
+++ src/test/java/com/example/demo/test/user/UpdateUser2.java
@@ -0,0 +1,26 @@
+package com.example.demo.test.user;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.UserRepository;
+
+@SpringBootTest
+public class UpdateUser2 {
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	void changePassword() {
+	//int rowcount= userRepository.updatePasswordByIdNative(1L, "1234");
+		int rowcount = userRepository.updatePasswordById(1L, "5678");
+				
+		System.out.println("rowcount = "+ rowcount);
+		 System.out.println(rowcount== 0 ? "修改失敗":"修改成功");
+	}
+	
+}
